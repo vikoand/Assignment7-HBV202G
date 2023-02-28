c349e2c31d4ad284c9758344603edf005c6b4b9c
vikoand <voa6@hi.is>
2023-02-28 11:09:40 +0000

    test setup

---
+++ src/test/java/rockPaperScissorsGameTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.example.rockPaperScissorsGame;
+import org.junit.Test;
+
+public class rockPaperScissorsGameTest {
+
+    /**
+    @Test
+    public void testGetUserChoice() {
+        rockPaperScissorsGame game = new rockPaperScissorsGame();
+        int userChoice = game.getUserChoice();
+        assertEquals(1, userChoice);
+    }
+    **/
+
+}
